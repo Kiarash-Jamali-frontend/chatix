d6c28e6da5ed8614e7e080912f530cc301a23e50
Kiarash jamali <kiarashjamali28@gmail.com>
2024-07-24 12:45:28 +0330

    Ability to see the number of unread messages in real time in the Sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,6 +5,7 @@ import {
   doc,
   getDoc,
   getDocs,
+  onSnapshot,
   or,
   query,
   where,
@@ -118,37 +119,8 @@ const Sidebar: React.FC = () => {
           </div>
         </div>
         <div>
-          {chats.map((c) => {
-            return (
-              <Link
-                to={`/chat/${c.phone}`}
-                className="flex items-center justify-between rounded-xl border-t hover:bg-base text-sm px-5 py-3"
-                key={c.phone}
-              >
-                <div className="flex items-center">
-                  {/*Profile image*/}
-                  {c.image ? (
-                    <img
-                      src={c.image}
-                      alt={"profile"}
-                      className="w-[40px] h-[40px] object-cover rounded-full"
-                    />
-                  ) : (
-                    <div className="w-[40px] h-[40px] border-2 rounded-full bg-base flex items-center justify-center">
-                      <FontAwesomeIcon icon={faUser} size="lg" />
-                    </div>
-                  )}
-                  <div className="ps-2">
-                    <div>{c.name}</div>
-                  </div>
-                </div>
-                {c.notSeenedMessages && (
-                  <div className="w-[20px] h-[20px] text-xs font-bold rounded-full bg-red-500 text-white text-center flex items-center justify-center">
-                    {c.notSeenedMessages}
-                  </div>
-                )}
-              </Link>
-            );
+          {chats.map((c, index) => {
+            return <ChatListItem chat={c} key={index} />;
           })}
         </div>
         <div className="p-6 mt-auto">
@@ -168,6 +140,64 @@ const Sidebar: React.FC = () => {
       </div>
     );
   }
+};
+
+type PropTypes = {
+  chat: any;
+};
+
+const ChatListItem: React.FC<PropTypes> = ({ chat }) => {
+  const [unreadMessagesCount, setUnreadMessagesCount] = useState<number>(0);
+
+  useEffect(() => {
+    const q = query(
+      collection(db, "chat_message"),
+      where("from", "==", chat.phone),
+      where("seen", "==", false)
+    );
+    const unsubscribe = onSnapshot(q, (querySnapshot) => {
+      setUnreadMessagesCount(querySnapshot.size);
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  return (
+    <>
+      <Link
+        to={`/chat/${chat.phone}`}
+        className="flex items-center justify-between border-t hover:bg-base text-sm px-5 py-3"
+        key={chat.phone}
+      >
+        <div className="flex items-center">
+          {/*Profile image*/}
+          {chat.image ? (
+            <img
+              src={chat.image}
+              alt={"profile"}
+              className="w-[40px] h-[40px] object-cover rounded-full"
+            />
+          ) : (
+            <div className="w-[40px] h-[40px] border-2 rounded-full bg-base flex items-center justify-center">
+              <FontAwesomeIcon icon={faUser} size="lg" />
+            </div>
+          )}
+          <div className="ps-2">
+            <div>{chat.name}</div>
+          </div>
+        </div>
+        {unreadMessagesCount ? (
+          <div className="w-[20px] h-[20px] text-xs font-bold rounded-full bg-red-500 text-white text-center flex items-center justify-center">
+            {unreadMessagesCount}
+          </div>
+        ) : (
+          ""
+        )}
+      </Link>
+    </>
+  );
 };
 
 export default Sidebar;
