c8bef5468ac1b84e22dc2a55a95bdb194f75034a
DARLING_BLUEMOON <kiajamali978@gmail.com>
2025-01-21 11:04:40 +0330

    I MISS UserLastActivity.tsx FILE

---
+++ src/components/UserLastActivity.tsx
@@ -0,0 +1,25 @@
+import { Timestamp } from "firebase/firestore";
+import { formatRelative, subDays } from "date-fns";
+import Profile from "../types/Profile";
+import useUserIsOnline from "../hooks/useUserIsOnline";
+
+export default function UserLastActivity({ profile }: { profile: Profile }) {
+    const userIsOnline = useUserIsOnline(profile.lastActivity);
+    return (
+        <>
+            {
+                userIsOnline ? (
+                    <span className="text-blue-500 text-xs font-medium inline-block">
+                        Online
+                    </span>
+                ) : <>
+                    <span className="text-xs inline-block text-black/60">
+                        {formatRelative(subDays(
+                            new Date(Timestamp.fromMillis(profile.lastActivity.seconds * 10 ** 3).toDate()), 0),
+                            new Date())}
+                    </span>
+                </>
+            }
+        </>
+    )
+}
