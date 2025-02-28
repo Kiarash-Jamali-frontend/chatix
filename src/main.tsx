ebdb860914b13e625c4405b4341a4dd2a0cc6130
DARLING_BLUEMOON <kiajamali978@gmail.com>
2025-02-28 17:41:24 +0330

    fix service worker update problem

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
+import { registerSW } from 'virtual:pwa-register'
 import "./index.css";
+
+registerSW({
+    immediate: true,
+})
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
  
