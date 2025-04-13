91c08b839a2c52f6276aae41fe68b465f7a47849
Darling_bluemoon <kiarash.jamali.frontend@gmail.com>
2025-04-13 17:47:37 +0330

    disable notification button

--- public/onesignal.js
+++ public/onesignal.js
@@ -4,7 +4,7 @@ OneSignalDeferred.push(async function (OneSignal) {
         appId: "8279bc50-2fb0-466b-a5eb-1661b14022a2",
         safari_web_id: "web.onesignal.auto.4f832ce8-c167-4c63-9514-5546a8912edb",
         notifyButton: {
-            enable: true,
+            enable: false,
         },
     });
 });
