15b5d59ff260b60fb2c677695936e0f85578925c
Darling_bluemoon <kiarash.jamali.frontend@gmail.com>
2025-05-22 17:22:36 +0330

    Change logo

--- pwa-assets.config.ts
+++ pwa-assets.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, minimal2023Preset } from '@vite-pwa/assets-generator/config'
 
 export default defineConfig({
-  preset: minimal2023Preset,
+  preset: { ...minimal2023Preset, maskable: { padding: 0, ...minimal2023Preset.maskable } },
   images: [
     'public/Logo.png',
   ],
