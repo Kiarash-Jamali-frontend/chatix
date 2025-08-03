e2ade735dcaf6c6041cc8bb20c52385b0ba6ce80
Darling_bluemoon <kiarash.jamali.frontend@gmail.com>
2025-08-03 14:25:54 +0330

    change cors config in server

--- server/api/index.cjs
+++ server/api/index.cjs
@@ -7,7 +7,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: "https://chatix-blush.vercel.app",
+  origin: "https://chatix-blush.vercel.app/",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
