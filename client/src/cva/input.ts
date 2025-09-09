1d5a675e8080aa52bb070977744de750af099c9e
Darling_bluemoon <kiarash.jamali.frontend@gmail.com>
2025-09-09 14:40:25 +0330

    change input style (when user focus on it)

--- client/src/cva/input.ts
+++ client/src/cva/input.ts
@@ -1,9 +1,9 @@
 import { cva } from "class-variance-authority";
 
-const input = cva("block w-full max-w-none outline-hidden font-Vazir", {
+const input = cva("block w-full max-w-none outline-hidden font-Vazir transition-all", {
     variants: {
         intent: {
-            default: "border bg-secondary focus:border-natural/15"
+            default: "border bg-secondary focus:border-natural/15 focus:ring focus:ring-offset-0 ring-natural/15"
         },
         size: {
             medium: "p-2 rounded-lg text-sm"
