cbaf060b2a8c91fae360b9eaf215d00592b217a4
Darling_bluemoon <kiarash.jamali.frontend@gmail.com>
2025-10-14 17:23:58 +0330

    fix notifications bug

--- client/src/Layout.tsx
+++ client/src/Layout.tsx
@@ -20,7 +20,6 @@ import { Unsubscribe } from "firebase/firestore";
 import { Draft, setDraftsList } from "./redux/slices/drafts";
 import { changeFontSize } from "./redux/slices/fontSize";
 import firestoreDefaultDBAPIUrl from "./constants/firestoreDefaultDBAPIUrl";
-import OneSignal from "react-onesignal";
 
 const Layout: React.FC = () => {
   const dispatch = useAppDispatch();
