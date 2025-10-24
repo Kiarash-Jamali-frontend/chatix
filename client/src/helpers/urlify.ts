54fb9ff24adf66157ff844fda395e937406573d4
Darling_bluemoon <kiarash.jamali.frontend@gmail.com>
2025-10-24 17:29:05 +0330

    fix bugs

--- client/src/helpers/urlify.ts
+++ client/src/helpers/urlify.ts
@@ -1,7 +1,11 @@
 import urlRegex from "url-regex";
 
 export default function urlify(string: string) {
-    return string.replace(urlRegex({exact: true, strict: false}), function (url) {
+    return string.replace(urlRegex({ exact: true, strict: false }), function (url) {
+        // Don't treat as URL if it contains @ symbol
+        if (url.includes('@')) {
+            return url;
+        }
         return `<a className="wrap-anywhere" target="_blank" href="${url}">${url}</a>`
     });
 }
