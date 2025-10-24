bb6a8322bf86d3afc6dc27285ef9b30a4b707e75
Darling_bluemoon <kiarash.jamali.frontend@gmail.com>
2025-10-24 14:50:06 +0330

    fix bugs

--- client/src/helpers/urlify.ts
+++ client/src/helpers/urlify.ts
@@ -2,6 +2,6 @@ import urlRegex from "url-regex";
 
 export default function urlify(string: string) {
     return string.replace(urlRegex({exact: true, strict: false}), function (url) {
-        return `<a target="_blank" href="${url}">${url}</a>`
+        return `<a className="wrap-anywhere" target="_blank" href="${url}">${url}</a>`
     });
 }
